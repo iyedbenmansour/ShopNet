138850326b907af92bd9154cbcd7f120312ead83
iyedbenmansour <tniyed@gmail.com>
2024-11-06 14:55:50 +0100

    deployement

--- server/config/shopnet.config.js
+++ server/config/shopnet.config.js
@@ -1,7 +1,8 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const db=process.env.db
+// MongoDB Atlas connection string (replace <db_password> with the actual password)
+const mongoURI = `mongodb+srv://shopnet:Aeazkmi123@shopnet.bgvsa.mongodb.net/shopnet?retryWrites=true&w=majority&appName=shopnet`;
 
-mongoose.connect(`mongodb://localhost:27017/${db}`)
-.then(()=>console.log(`connected to ${db} db`))
-.catch(err=>console.log(err))
+mongoose.connect(mongoURI)
+  .then(() => console.log("Connected to shopnet database on MongoDB Atlas"))
+  .catch((err) => console.log("Error connecting to MongoDB Atlas:", err));
